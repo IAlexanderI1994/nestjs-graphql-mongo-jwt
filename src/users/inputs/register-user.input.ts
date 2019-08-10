c2614389657c57bc53d28869720be8144b59c96a
blaze <kontaktAK@Yandex.ru>
2019-08-10 19:07:22 +0300

    refactoring

--- src/users/inputs/register-user.input.ts
+++ src/users/inputs/register-user.input.ts
@@ -8,13 +8,13 @@ export class RegisterUserInput {
   readonly email : string
 
   @Field()
-  @MinLength(60)
-  @MaxLength(200)
+  @MinLength(6)
+  @MaxLength(30)
   readonly password : string
 
-  @Field({ nullable: true })
-  @MinLength(60)
-  @MaxLength(200)
-  readonly password2? : string
+  @Field() //{ nullable: true }
+  @MinLength(6)
+  @MaxLength(30)
+  readonly password2 : string
 
 }
